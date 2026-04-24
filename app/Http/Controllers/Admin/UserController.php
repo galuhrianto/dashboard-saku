98ac20ae5e9ca67e616bd7db2a3e1b6e1f4fb3af
galuhrianto <galuhrianto12@gmail.com>
2026-04-24 10:07:32 +0700

    revisi

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Role;
 use App\Models\User;
-use App\Models\Backupreceiver;
+use App\Models\BackupReceiver;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
